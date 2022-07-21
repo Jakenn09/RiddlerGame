cb9549c7e2cdfcdb034c42077ab911a2b6744579
Jakenn09 <88737390+Jakenn09@users.noreply.github.com>
2022-07-21 16:59:08 -0400

    massive changes to the main game

--- riddler_game.py
+++ riddler_game.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import time
+from pygame_functions import *
 from button import Button
 
 #Initializes all imported pygame modules
@@ -39,10 +41,29 @@ game_instructions = pygame.transform.scale(game_instructions, (450, 250))
 redtextcolor = (250, 0, 0)
 
 #Level and lives
-level = 1
-lives = 3
+
 level_lives_font =  pygame.font.SysFont('comicsans', 25)
 whitetextcolor = (255, 255, 255)
+lives_coord = (400,200)
+level_coord = (30, 200)
+
+#riddle font
+riddle_font = pygame.font.SysFont('comicsans', 15)
+riddle_coord = (150, 350)
+
+# Textbox
+input_box = pygame.Rect(150, 450, 140, 32)
+user_text = ''
+
+answers = ["an umbrella", "a rubber band", "a stamp", "a mushroom", "the letter g", 
+                "nothing", "light", "jared", "a piano", "a clock", "a mountain", "wind", "time","teeth", "an egg"]
+
+riddles = ["What can you catch, but not throw?", "What kind of band never plays music?", "What can travel around the world without leaving its corner?",
+            "What kind of room has no doors or windows?", "What is the end of everything?", "Poor People have it. Rich people need it. If you eat it you die. What is it?",
+            "What can fill a room but takes up no space?", "Jared’s father has three sons: Snap, Crackle, and..?", "What has many keys but can’t open a single lock?",
+            "What has hands but can’t clap?", "What has roots as nobody sees, Is taller than trees, Up, up it goes, And yet never grows?", "Voiceless it cries, Wingless flutters, Toothless bites, Mouthless mutters",
+             "This thing all things devours: Birds, beasts, trees, flowers; Gnaws iron, bites steel; Grinds hard stones to meal; Slays king, ruins town, And beats high mountain down.",
+             "Thirty white horses on a red hill, First they champ, Then they stamp, Then they stand still", "A box without hinges, key, or lid, Yet golden treasure inside is hid"]
 
 def main_menu():
     pygame.display.set_caption("Main Menu")
@@ -145,6 +166,24 @@ def play():
 
 
 
+'''
+def game_loop():
+    level = 1
+    lives = 3
+    
+    lives_lable = level_lives_font.render(f"Lives: {lives}", 1, whitetextcolor)
+    level_label = level_lives_font.render(f"Level: {level}", 1, whitetextcolor)
+    screen.blit(lives_lable, lives_coord)
+    game_riddle = riddle_font.render(riddles[0], 1, whitetextcolor)
+    screen.blit(game_riddle, riddle_coord)
+    pygame.display.update()
+    for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+    pygame.display.update()  
+'''
+
 def main_game():
     pygame.display.set_caption("The Riddler Game")
 
@@ -159,19 +198,25 @@ def main_game():
 
             screen.blit(play_text, play_rect)
 
+            level = 1
+            lives = 3
+    
             lives_lable = level_lives_font.render(f"Lives: {lives}", 1, whitetextcolor)
             level_label = level_lives_font.render(f"Level: {level}", 1, whitetextcolor)
+            screen.blit(lives_lable, lives_coord)
+            screen.blit(level_label, level_coord)
 
-            screen.blit(lives_lable, (400, 200))
-            screen.blit(level_label, (30, 200))
 
-            pygame.display.update() 
+            game_riddle = riddle_font.render(riddles[0], 1, whitetextcolor)
+            screen.blit(game_riddle, riddle_coord)
+            
+            pygame.display.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             pygame.display.update()        
-                        
+            
 print("Welcome to The Riddler Game")
 main_menu()       
