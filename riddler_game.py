3c8500a3a028c03afd5ffc7d94c03556e99233e1
Jakenn09 <88737390+Jakenn09@users.noreply.github.com>
2022-07-21 18:06:47 -0400

    added comments to most function
    need to work on game loop as the screen freezes waiting on text input

--- riddler_game.py
+++ riddler_game.py
@@ -19,7 +19,7 @@ screen_width, screen_height = 1080, 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Main Menu")
 
-#fonts for GUI
+# fonts and color for GUI
 mainfont = pygame.font.SysFont('comicsans', 100)
 smallfont = pygame.font.SysFont('comicsans', 35)
 redtextcolor = (255, 0, 0)
@@ -40,24 +40,25 @@ game_instructions = pygame.transform.scale(game_instructions, (450, 250))
 #main text color (Red)
 redtextcolor = (250, 0, 0)
 
-#Level and lives
-
+# Level and lives with font, color and coordinates for GUI
 level_lives_font =  pygame.font.SysFont('comicsans', 25)
 whitetextcolor = (255, 255, 255)
 lives_coord = (400,200)
 level_coord = (30, 200)
 
-#riddle font
+# Riddle font and coordinates for GUI
 riddle_font = pygame.font.SysFont('comicsans', 15)
 riddle_coord = (150, 350)
 
-# Textbox
+# Textbox (might not be used)
 input_box = pygame.Rect(150, 450, 140, 32)
 user_text = ''
 
+# List containing the answers to the riddles
 answers = ["an umbrella", "a rubber band", "a stamp", "a mushroom", "the letter g", 
                 "nothing", "light", "jared", "a piano", "a clock", "a mountain", "wind", "time","teeth", "an egg"]
 
+# List containing the riddles
 riddles = ["What can you catch, but not throw?", "What kind of band never plays music?", "What can travel around the world without leaving its corner?",
             "What kind of room has no doors or windows?", "What is the end of everything?", "Poor People have it. Rich people need it. If you eat it you die. What is it?",
             "What can fill a room but takes up no space?", "Jared’s father has three sons: Snap, Crackle, and..?", "What has many keys but can’t open a single lock?",
@@ -65,47 +66,48 @@ riddles = ["What can you catch, but not throw?", "What kind of band never plays 
              "This thing all things devours: Birds, beasts, trees, flowers; Gnaws iron, bites steel; Grinds hard stones to meal; Slays king, ruins town, And beats high mountain down.",
              "Thirty white horses on a red hill, First they champ, Then they stamp, Then they stand still", "A box without hinges, key, or lid, Yet golden treasure inside is hid"]
 
+# Main menu function 
 def main_menu():
-    pygame.display.set_caption("Main Menu")
+    pygame.display.set_caption("Main Menu") 
     screen = pygame.display.set_mode((screen_width, screen_height))
 
     while running:
-        screen.blit(back_ground, (0, 0)) #places background on screen
+        screen.blit(back_ground, (0, 0)) # places background on screen
 
-        menu_mouse_pos = pygame.mouse.get_pos() #gets the mouses position
+        menu_mouse_pos = pygame.mouse.get_pos() # gets the mouses position
 
-        menu_text = mainfont.render("The Riddler Game", True, redtextcolor) #creates text and makes color red
-        menu_rect = menu_text.get_rect(center = (540, 100)) #centers the text
+        menu_text = mainfont.render("The Riddler Game", True, redtextcolor) # creates text and makes color red
+        menu_rect = menu_text.get_rect(center = (540, 100)) # centers the text
 
         start_button = Button(image = button_image, pos = (150, 250), 
-                            text_input = "Next", font = smallfont, base_color = "#d7fcd4", hovering_color = "Red" )
+                            text_input = "Next", font = smallfont, base_color = "#d7fcd4", hovering_color = "Red" ) # creation of the start button
         instructions_button = Button(image = button_image , pos = (150, 350), 
-                            text_input = "Instructions", font = smallfont, base_color = "#d7fcd4", hovering_color = "Red")
+                            text_input = "Instructions", font = smallfont, base_color = "#d7fcd4", hovering_color = "Red") # creation of the instructions button 
 
-        screen.blit(menu_text, menu_rect)
+        screen.blit(menu_text, menu_rect) # adds the menu text and rectangle to the screen
 
         for button in [start_button, instructions_button]:
-            button.changeColor(menu_mouse_pos)
-            button.update(screen)
+            button.changeColor(menu_mouse_pos) # changes color of button when highlighted
+            button.update(screen) 
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                pygame.quit() # exits GUI when red X is pressed
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: # if mouse is pressed on start button, play function runs 
                 if start_button.checkForInput(menu_mouse_pos):
                     play()
-                if instructions_button.checkForInput(menu_mouse_pos):
+                if instructions_button.checkForInput(menu_mouse_pos): # if mouse is pressed on instructions button, instructions function is run
                     instructions()
-        pygame.display.update()
+        pygame.display.update() # updates GUI on button press
 
 def instructions():
     pygame.display.set_caption("Instructions")
 
     while running:
-        screen.fill("black")
+        screen.fill("black") # Sets screen and background
         screen.blit(back_ground, (0,0))
-        screen.blit(game_instructions, (100, 300))
+        screen.blit(game_instructions, (100, 300)) # adds the instruction .png to the GUI
 
         instructions_mouse_pos = pygame.mouse.get_pos()
 
@@ -115,18 +117,18 @@ def instructions():
         menu_button = Button(image = button_image, pos = (540, 650),
                     text_input = "Main Menu", font = smallfont, base_color = "#d7fcd4", hovering_color = "Red")
 
-        screen.blit(instructions_text, instructions_rect)
+        screen.blit(instructions_text, instructions_rect) # adds instructions text to the screen
 
         for button in [menu_button]:
-            button.changeColor(instructions_mouse_pos)
+            button.changeColor(instructions_mouse_pos) # changes color of button when highlighted
             button.update(screen)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                pygame.quit() # exits GUI when red X is pressed
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if menu_button.checkForInput(instructions_mouse_pos):
+                if menu_button.checkForInput(instructions_mouse_pos): # if menu button is pressed, takes user back to main menu function 
                     main_menu()
         pygame.display.update()
 
@@ -134,7 +136,7 @@ def play():
         pygame.display.set_caption("The Riddler Game")
 
         while running: 
-            screen.fill("black")
+            screen.fill("black") # Sets default screen
             screen.blit(back_ground, (0, 0))
 
             play_mouse_pos = pygame.mouse.get_pos()
@@ -143,25 +145,25 @@ def play():
             play_rect = play_text.get_rect(center = (540, 100))
 
             start_game_button = Button(image = button_image, pos = (540, 250),
-                            text_input = "Start Game", font = smallfont, base_color ="#d7fcd4", hovering_color = "Red")
+                            text_input = "Start Game", font = smallfont, base_color ="#d7fcd4", hovering_color = "Red") # creation of start game button 
             menu_button = Button(image = button_image, pos = (540, 650),
-                            text_input = "Main Menu", font = smallfont, base_color ="#d7fcd4", hovering_color = "Red")
+                            text_input = "Main Menu", font = smallfont, base_color ="#d7fcd4", hovering_color = "Red") # creation of main menu button 
 
-            screen.blit(play_text, play_rect)
+            screen.blit(play_text, play_rect) # adds the pplay text to the screen
 
             for button in [start_game_button, menu_button]:
-                button.changeColor(play_mouse_pos)
+                button.changeColor(play_mouse_pos) # changes color of button when highlighted
                 button.update(screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
+                    pygame.quit() # exits GUI when red X is pressed
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if menu_button.checkForInput(play_mouse_pos):
+                    if menu_button.checkForInput(play_mouse_pos): # if menu button is pressed, takes user back to main menu
                         main_menu()
-                    if start_game_button.checkForInput(play_mouse_pos):
-                        main_game()
+                    if start_game_button.checkForInput(play_mouse_pos): # if start game button is pressed, takes you to the main game loop
+                        main_game() 
             pygame.display.update()
 
 
@@ -188,7 +190,7 @@ def main_game():
     pygame.display.set_caption("The Riddler Game")
 
     while running: 
-            screen.fill("black")
+            screen.fill("black") # sets screen to default
             screen.blit(back_ground, (0, 0))
 
             play_mouse_pos = pygame.mouse.get_pos()
@@ -196,22 +198,33 @@ def main_game():
             play_text = mainfont.render("Riddler's Games", True, redtextcolor)
             play_rect = play_text.get_rect(center = (540, 100))
 
+            
             screen.blit(play_text, play_rect)
 
             level = 1
             lives = 3
-    
-            lives_lable = level_lives_font.render(f"Lives: {lives}", 1, whitetextcolor)
-            level_label = level_lives_font.render(f"Level: {level}", 1, whitetextcolor)
-            screen.blit(lives_lable, lives_coord)
-            screen.blit(level_label, level_coord)
-
-
-            game_riddle = riddle_font.render(riddles[0], 1, whitetextcolor)
-            screen.blit(game_riddle, riddle_coord)
             
-            pygame.display.update()
+            for i in range(len(answers)):
+                lives_lable = level_lives_font.render(f"Lives: {lives}", 1, whitetextcolor)
+                level_label = level_lives_font.render(f"Level: {level}", 1, whitetextcolor)
+                screen.blit(lives_lable, lives_coord)
+                screen.blit(level_label, level_coord)
 
+
+                game_riddle = riddle_font.render(riddles[i], 1, whitetextcolor)
+                screen.blit(game_riddle, riddle_coord)
+            
+                pygame.display.update()
+                user_answer = input("Enter Answer Here: ")
+                if user_answer == answers[i]:
+                    level = level + 1
+                    if level >= 10:
+                        break
+                else:
+                    level = level + 1
+                    lives = lives + 1
+                    if lives == 0:
+                        break
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
